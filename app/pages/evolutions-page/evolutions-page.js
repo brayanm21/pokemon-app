84875ddf0c30a89ae2833d3408db786a542424d4
brayan muñoz <bmunozmasia@uniminuto.edu.co>
2024-10-29 17:52:59 -0500

    Primer comit con la union de los componentes

---
+++ app/pages/evolutions-page/evolutions-page.js
@@ -0,0 +1,55 @@
+import { CellsPage } from '@cells/cells-page';
+import { BbvaCoreIntlMixin } from '@bbva-web-components/bbva-core-intl-mixin';
+import { html, css } from 'lit-element';
+
+import '@cells-components/cells-template-paper-drawer-panel';
+import '@meraki/evolution-ui/evolution-ui.js';
+
+
+/* eslint-disable new-cap */
+class EvolutionsPage extends BbvaCoreIntlMixin(CellsPage) {
+  static get is() {
+    return 'evolutions-page';
+  }
+
+  static get properties() {
+    return {
+      idNumber: { type: String }
+    };
+  }
+
+  constructor() {
+    super();
+    this.idNumber = '2';
+  }
+
+  render() {
+    return html`
+      <cells-template-paper-drawer-panel mode="seamed">
+        <div slot="app__header">
+          <bbva-header-main
+            icon-left-primary="coronita:return-12"
+            accessibility-text-icon-left-primary="Volver"
+            image="https://1000marcas.net/wp-content/uploads/2020/01/Pok%C3%A9mon-emblema.jpg"
+            @header-main-icon-left-primary-click=${() => window.history.back()}>
+          </bbva-header-main>
+        </div>
+
+        <div slot="app__main" class="container">
+        <evolution-ui
+          id =${this.idNumber}
+        ></evolution-ui>
+        </div>
+     </cells-template-paper-drawer-panel>`;
+  }
+  static get styles() {
+    return css`
+      bbva-header-main {
+        --bbva-header-main-bg-color: #000000;
+        
+      }
+      `;
+  }
+}
+
+window.customElements.define(EvolutionsPage.is, EvolutionsPage);
---
+++ app/pages/pokemon-page/pokemon-page.js
@@ -0,0 +1,62 @@
+import { CellsPage } from '@cells/cells-page';
+import { BbvaCoreIntlMixin } from '@bbva-web-components/bbva-core-intl-mixin';
+import { html,css } from 'lit-element';
+
+import '@cells-components/cells-template-paper-drawer-panel';
+import '@bbva-web-components/bbva-header-main';
+import '@meraki/pokemon-ui/pokemon-ui.js';
+
+/* eslint-disable new-cap */
+class PokemonPage extends BbvaCoreIntlMixin(CellsPage) {
+  static get is() {
+    return 'pokemon-page';
+  }
+
+  static get properties() {
+    return {
+      title: { type: String }
+    };
+  }
+
+  constructor() {
+    super();
+    this.title = 'holamundo';
+  }
+
+  handleMovementClick({ id, label }) {
+    this.publish('movement_title', label);
+    this.navigate('movement-detail', { id, label: normalizeUriString(label), });
+  }
+
+  render() {
+    return html`
+      <cells-template-paper-drawer-panel mode="seamed">
+        <div slot="app__header">
+          <bbva-header-main
+            image="https://1000marcas.net/wp-content/uploads/2020/01/Pok%C3%A9mon-emblema.jpg"
+          </bbva-header-main>
+        </div>
+
+        <div slot="app__main" class="container">
+        <bbva-button-default 
+              text="Details" 
+              @click="${() => this.navigate('evolutions')}">
+            </bbva-button-default>
+          <pokemon-ui></pokemon-ui>
+        </div>
+     </cells-template-paper-drawer-panel>`;
+  }
+
+  static get styles() {
+    return css`
+      bbva-header-main {
+        --bbva-header-main-bg-color: #000000;
+      }
+      .container {
+        background-color: black;
+      }
+      `;
+  }
+}
+
+window.customElements.define(PokemonPage.is, PokemonPage);
